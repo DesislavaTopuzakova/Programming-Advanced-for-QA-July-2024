8ffd461c77251c6783023cb704ab5ba73a77e295
Desislava Topuzakova <34713586+DesislavaTopuzakova@users.noreply.github.com>
2024-07-16 11:45:38 +0300

    Dictionaries - Lab

---
+++ 04-Dictionaries-Lambda-LINQ/Solutions/CountRealNumbers_01/Program.cs
@@ -0,0 +1,34 @@
+﻿//входни данни -> масив от дробни числа
+double [] numbers = Console.ReadLine()      //"8 2 2 8 2"
+                    .Split(" ")             //["8", "2", "2", "8", "2"]
+                    .Select(double.Parse)   //[8, 2, 2, 8, 2]
+                    .ToArray();
+
+//key (число) -> value (бр. срещания)
+SortedDictionary<double, int> countNumbers = new SortedDictionary<double, int>();
+
+
+foreach(double number in numbers)
+{
+    //проверка дали това число вече е срещано
+    if (countNumbers.ContainsKey(number))
+    {
+        //вече имам записано числото с някакъв брой срещания
+        //увеличаваме броя на срещанията с 1
+        countNumbers[number]++;
+    }
+    else
+    {
+        //не сме срещали моето число до момента -> срещаме за първи път
+        countNumbers.Add(number, 1);
+    }
+}
+
+//запис: key (число) -> value (бр. срещания)
+foreach (KeyValuePair<double, int> entry in countNumbers)
+{
+    //всеки един се запис се съхранява в entry
+    //entry.Key -> число
+    //entry.Value -> бр. срещания
+    Console.WriteLine(entry.Key + " -> " + entry.Value);
+}
---
+++ 04-Dictionaries-Lambda-LINQ/Solutions/WordSynonyms_03/Program.cs
@@ -0,0 +1,33 @@
+﻿//запис: key (дума) -> value (списък със синоними)
+
+Dictionary<string, List<string>> wordSynonyms = new Dictionary<string, List<string>>();
+
+int countWords = int.Parse(Console.ReadLine()); //брой на буквите
+
+
+for (int count = 1; count <= countWords; count++)
+{
+    string word = Console.ReadLine(); //стойността на думата
+    string synonym = Console.ReadLine(); //синоним на думата
+
+    if (!wordSynonyms.ContainsKey(word))
+    {
+        //за първи път срещаме думата
+        wordSynonyms.Add(word, new List<string>());
+        wordSynonyms[word].Add(synonym);
+    }
+    else
+    {
+        //вече сме срещали думата -> добавяме новия синоним към текущия списък със синоними
+        wordSynonyms[word].Add(synonym);
+    }
+}
+
+//запис: key (дума) -> value (списък със синонимите)
+foreach (KeyValuePair<string, List<string>> entry in wordSynonyms)
+{
+    //entry
+    //entry.Key -> дума (string)
+    //entry.Value -> списък със синоними (List<string>)
+    Console.WriteLine(entry.Key + " - " + string.Join(", ", entry.Value));
+}
