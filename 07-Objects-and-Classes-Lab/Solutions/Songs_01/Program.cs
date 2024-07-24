50d47a1c383a956516e64e23ed47b97f25106310
Desislava Topuzakova <34713586+DesislavaTopuzakova@users.noreply.github.com>
2024-07-24 10:18:26 +0300

    Objects and Classes - Lab

---
+++ 07-Objects-and-Classes-Lab/Solutions/Songs_01/Program.cs
@@ -0,0 +1,38 @@
+﻿//логиката на изпълнение на задачата
+
+//входни данни
+int countSongs = int.Parse(Console.ReadLine()); //брой на песните
+
+//лист, в който ще съхранявам песните
+List<Song> songsList = new List<Song>();
+
+for (int count = 1; count <= countSongs; count++)
+{
+    //въвеждам данни за песента
+    string data = Console.ReadLine();
+    string[] songData = data.Split("_");
+    //data = "favourite_DownTown_3:14".Split("_")
+    //-> songData = ["favourite", "DownTown", "3:14"]
+
+    string typeList = songData[0]; // "favourite"
+    string songName = songData[1]; // "DownTown"
+    string time = songData[2];     // "3:14"
+
+    //нова песен с въведените данни
+    Song song = new Song(typeList, songName, time);
+
+    //добавяме създадената песен в списъка
+    songsList.Add(song);
+}
+
+//списък с въведените песни
+
+string typeSongToPrint = Console.ReadLine(); //плейлист или "all"
+
+foreach (Song song in songsList)
+{
+    if (typeSongToPrint == "all" || typeSongToPrint == song.TypeList)
+    {
+        Console.WriteLine(song.Name);
+    }
+}
---
+++ 07-Objects-and-Classes-Lab/Solutions/Students_02/Student.cs
@@ -0,0 +1,29 @@
+﻿//описвам всеки един студент
+public class Student
+{
+   //част 1: характеристики -> описват чрез properties
+   public string FirstName { get; set; }
+   public string LastName { get; set; }
+   public int Age { get; set; }
+   public string HomeTown { get; set; }
+
+    //default конструктор -> създава празен обект: new Student()
+
+    //custom конструктор -> създаваме специфични обекти от класа
+    public Student(string firstName, string lastName, int age, string homeTown)
+    {
+        //нов празен обект
+        //FirstName = null
+        //LastName = null
+        //Age = 0
+        //HomeTown = null
+
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+        HomeTown = homeTown;
+    }
+
+}
+
+
